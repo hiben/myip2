dfad8a5d53bc9f5eceecd1776a6ecde79cf540e2
Hendrik Iben <hendrik.iben@gmail.com>
2025-03-06 23:36:20 +0100

    update gradle and dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,33 +1,22 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
 plugins {
-    kotlin("jvm") version "1.8.20"
-    id("com.github.johnrengelman.shadow") version "8.1.1"
+    kotlin("jvm") version "2.1.10"
+    id("com.gradleup.shadow") version "8.3.5"
     application
 }
 
 group = "de.zvxeb"
-version = "1.0-SNAPSHOT"
+version = "1.1-SNAPSHOT"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    implementation("io.javalin:javalin-bundle:5.1.1")
-    implementation("org.slf4j:slf4j-simple:2.0.3")
-    implementation("com.beust:jcommander:1.81")
-    testImplementation(kotlin("test"))
-    implementation(kotlin("stdlib-jdk8"))
-}
-
-tasks.test {
-    useJUnitPlatform()
-}
-
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "11"
+    implementation("io.javalin:javalin-bundle:6.4.0")
+    implementation("org.slf4j:slf4j-simple:2.0.17")
+    implementation("org.jcommander:jcommander:2.0")
 }
 
 application {
@@ -36,12 +25,4 @@ application {
 
 tasks.withType<ShadowJar> {
     archiveFileName.set("myip2.jar")
-}
-val compileKotlin: KotlinCompile by tasks
-compileKotlin.kotlinOptions {
-    jvmTarget = "11"
-}
-val compileTestKotlin: KotlinCompile by tasks
-compileTestKotlin.kotlinOptions {
-    jvmTarget = "11"
 }
